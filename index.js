db9b469ce7825ff2c65af5946227066442a38306
Vitor <devitorbadia@gmail.com>
2024-06-25 20:56:55 -0300

    update html, css and js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-/*
-t1 t2   t3 = 0 
-0 + 1 = 1
-1 + 1 = 2
-1 + 2 = 3
-2 + 3 = 5
-3 + 5 = 8
-*/
+function pageTheme() {
+    document.body.classList.toggle('is-dark')
+    document.body.classList.toggle('is-light')
+}
+
+document.getElementById('btn-dark/light').addEventListener('click', pageTheme)
+
 
 let x = 0
 let y = 1
@@ -14,40 +13,65 @@ let z = 0
 z = x + y
 
 let option = 0
-let valor = parseInt(prompt("digite um número:"))
 
+function calcFibonacci(valor) {
 
-function pageTheme() {
-    document.body.classList.toggle('is-light')
-    document.body.classList.toggle('is-dark')
-  }
-  
-  document.getElementById('btn-dark/light').addEventListener('click', pageTheme)
+    while (z <= valor) {
+        x = y
+        y = z
+        z = x + y
+        createLi(x, y, z)
 
-for (i = 0; i < valor; i++) {
-    x = y
-    y = z
-    z = x + y
-    console.log(x + " + " + y + " = " + z)
+        if (valor == z) {
+            option = 1
+        }
 
-    if(valor === z) {
-        option = 1
     }
-} 
 
-if(option === 1) {
-    console.log(valor + " é um valor de uma sequência fibonacci" )
+    if (option === 1) {
+        console.log(valor + " é um valor de uma sequência fibonacci")
+        zerarVariaveis()
+        return true
+    } else {
+        console.log("não há este valor na sequência de fibonacci " + "(" + valor + ")")
+        zerarVariaveis()
+        return false
+    }
+
 }
 
-else  {
-    console.log("não há este valor na sequência de fibonacci " + "(" + valor + ")")
+function zerarVariaveis() {
+    x = 0
+    y = 1
+    z = 0
+    option = 0
 }
-const fibonacciBtn = document.getElementById("fibonacciBtn")
 
-function fibonacciBtnActive () {
-    if (fibonacciBtn) {
-        fibonacciBtn = prompt("ola?")
+
+
+function fibonacciBtnActive() {
+    const ul = document.getElementById("numbersResult")
+    removeChilds(ul)
+    const inputValue = document.getElementById("fibonacci").value
+    const result = calcFibonacci(inputValue)
+
+    if (result) {
+        document.getElementById("result").textContent = "há este valor: " + inputValue + " na sequência de fibonacci"
+    } else {
+        document.getElementById("result").textContent = "não há este valor: " + inputValue + " na sequência de fibonacci"
     }
-} 
 
-/* a pessoa poder digitar no input e ao colocar o número checar se é ou não uma sequencia de fibonati (aparecendo na tela escrito) */
+}
+
+function createLi(x, y, z) {
+    const li = document.createElement("li")
+    li.innerText = x + " + " + y + " = " + z
+    li.className = "vitor"
+    document.getElementById("numbersResult").appendChild(li)
+}
+
+function removeChilds(parent) {
+    while(parent.lastChild) {
+        parent.removeChild(parent.lastChild)
+    }
+}
